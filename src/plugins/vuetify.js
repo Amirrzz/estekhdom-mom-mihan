e2b194adb0f96f24c7c6d1216d9dd3388e47d2a9
Amirrez <amirrez.call@gmail.com>
2024-06-15 13:57:00 +0330

    add font-awesome: ^4.7.0 and conifg in vuetify plugin

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,15 +5,26 @@
  */
 
 // Styles
+import 'font-awesome/css/font-awesome.min.css' // Ensure your project is capable of handling css files
+
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
 // Composables
 import { createVuetify } from 'vuetify'
+import { aliases, fa } from 'vuetify/iconsets/fa4'
+
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
     defaultTheme: 'dark',
+  },
+  icons: {
+    defaultSet: 'fa',
+    aliases,
+    sets: {
+      fa,
+    },
   },
 })
