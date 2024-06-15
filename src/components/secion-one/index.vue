7fa1ec445e625ef87c00316288a7633475bb5f13
Amirrez <amirrez.call@gmail.com>
2024-06-15 16:38:34 +0330

    implement slider and create account buttom

--- src/components/secion-one/index.vue
+++ src/components/secion-one/index.vue
@@ -1,137 +1,13 @@
 <template>
   <section class="section-one">
     <v-container class="fill-height">
-      <v-responsive class="align-centerfill-height mx-auto" max-width="900">
-        <v-row>
-          <v-col cols="12">
-            <v-card
-              class="py-4"
-              color="surface-variant"
-              image="https://cdn.vuetifyjs.com/docs/images/one/create/feature.png"
-              prepend-icon="mdi-rocket-launch-outline"
-              rounded="lg"
-              variant="outlined"
+      <v-responsive class="align-centerfill-height mx-auto">
+        <v-row class="justify-center ga-6">
+          <v-col cols="12"><Slider></Slider></v-col>
+          <v-col cols="12" md="3" sm="6" class="text-center">
+            <v-btn rounded="xl" size="x-large" color="#482BE7" variant="flat"
+              >Create an Account</v-btn
             >
-              <template #image>
-                <v-img position="top right" />
-              </template>
-
-              <template #title>
-                <h2 class="text-h5 font-weight-bold">Get started</h2>
-              </template>
-
-              <template #subtitle>
-                <div class="text-subtitle-1">
-                  Change this page by updating
-                  <v-kbd>{{ `<HelloWorld />` }}</v-kbd> in
-                  <v-kbd>components/HelloWorld.vue</v-kbd>.
-                </div>
-              </template>
-
-              <v-overlay
-                opacity=".12"
-                scrim="primary"
-                contained
-                model-value
-                persistent
-              />
-            </v-card>
-          </v-col>
-
-          <v-col cols="6">
-            <v-card
-              append-icon="mdi-open-in-new"
-              class="py-4"
-              color="surface-variant"
-              href="https://vuetifyjs.com/"
-              prepend-icon="mdi-text-box-outline"
-              rel="noopener noreferrer"
-              rounded="lg"
-              subtitle="Learn about all things Vuetify in our documentation."
-              target="_blank"
-              title="Documentation"
-              variant="text"
-            >
-              <v-overlay
-                opacity=".06"
-                scrim="primary"
-                contained
-                model-value
-                persistent
-              />
-            </v-card>
-          </v-col>
-
-          <v-col cols="6">
-            <v-card
-              append-icon="mdi-open-in-new"
-              class="py-4"
-              color="surface-variant"
-              href="https://vuetifyjs.com/introduction/why-vuetify/#feature-guides"
-              prepend-icon="mdi-star-circle-outline"
-              rel="noopener noreferrer"
-              rounded="lg"
-              subtitle="Explore available framework Features."
-              target="_blank"
-              title="Features"
-              variant="text"
-            >
-              <v-overlay
-                opacity=".06"
-                scrim="primary"
-                contained
-                model-value
-                persistent
-              />
-            </v-card>
-          </v-col>
-
-          <v-col cols="6">
-            <v-card
-              append-icon="mdi-open-in-new"
-              class="py-4"
-              color="surface-variant"
-              href="https://vuetifyjs.com/components/all"
-              prepend-icon="mdi-widgets-outline"
-              rel="noopener noreferrer"
-              rounded="lg"
-              subtitle="Discover components in the API Explorer."
-              target="_blank"
-              title="Components"
-              variant="text"
-            >
-              <v-overlay
-                opacity=".06"
-                scrim="primary"
-                contained
-                model-value
-                persistent
-              />
-            </v-card>
-          </v-col>
-
-          <v-col cols="6">
-            <v-card
-              append-icon="mdi-open-in-new"
-              class="py-4"
-              color="surface-variant"
-              href="https://discord.vuetifyjs.com"
-              prepend-icon="mdi-account-group-outline"
-              rel="noopener noreferrer"
-              rounded="lg"
-              subtitle="Connect with Vuetify developers."
-              target="_blank"
-              title="Community"
-              variant="text"
-            >
-              <v-overlay
-                opacity=".06"
-                scrim="primary"
-                contained
-                model-value
-                persistent
-              />
-            </v-card>
           </v-col>
         </v-row>
       </v-responsive>
@@ -140,14 +16,13 @@
 </template>
 
 <script setup>
-//
+import Slider from "@/components/secion-one/slider.vue";
 </script>
 
 <style scoped>
 .section-one {
   background: linear-gradient(0deg, #2f189380, #2f189380),
     url("@/assets/section-one/section-one-background-image.jfif");
-
   height: 100%;
   background-size: cover;
   background-position: center;
---
+++ src/components/secion-one/slider.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-carousel height="400" hide-delimiter-background show-arrows>
+    <template v-slot:prev="{ props }">
+      <v-icon icon="fa-angle-left" @click="props.onClick" />
+    </template>
+    <template v-slot:next="{ props }">
+      <v-icon icon="fa-angle-right" @click="props.onClick" />
+    </template>
+    <template v-slot:delimiter:="{ props }"> {{ props }} </template>
+    <v-carousel-item v-for="(slide, i) in 5" :key="i">
+      <div class="slide-item text-center">
+        <div class="subheadr">Startup 3</div>
+        <div class="header">Forget About Code</div>
+        <div class="discription">
+          Startup Framework gives you complete freedom over your creative
+          process — you don’t have to think about any technical aspects. There
+          are no limits and absolutely no coding.
+        </div>
+      </div>
+    </v-carousel-item>
+  </v-carousel>
+</template>
+<style scoped>
+.slide-item {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 28px;
+  max-width: 736px;
+  margin: auto;
+}
+.subheadr {
+  font-size: 24px;
+}
+.header {
+  font-size: 72px;
+}
+.discription {
+  font-size: 22px;
+}
+</style>
+<style>
+.v-carousel .fa-circle:before {
+  content: "";
+  height: 10px;
+  width: 10px;
+  border-radius: 50%;
+  border: 2px solid white;
+}
+.v-carousel .v-btn--active .fa-circle:before {
+  background-color: white;
+}
+</style>
